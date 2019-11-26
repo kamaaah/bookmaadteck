bcfab39fedaf67da5f22f723b86ac0023a0e4691
kamaaah <x.argis@gmail.com>
2019-11-26 11:51:07 +0100

    création de l'authentification signup: serv + view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookmaadteck';
+
+  constructor() {
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyBEzCo2Qr0ULBI-dh5BJ5ZN26zx7A3bNv4",
+      authDomain: "bookmaadteck.firebaseapp.com",
+      databaseURL: "https://bookmaadteck.firebaseio.com",
+      projectId: "bookmaadteck",
+      storageBucket: "bookmaadteck.appspot.com",
+      messagingSenderId: "408861503052",
+      appId: "1:408861503052:web:19f0ce65f21cb4aa1b8ee9"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { reject } from 'q';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() {}
+
+    createNewUser(email: string, password: string) {
+      return new Promise(
+        // tslint:disable-next-line: no-shadowed-variable
+        (resolve, reject) => {
+          firebase.auth ().createUserWithEmailAndPassword(email, password).then(
+            () => {
+              resolve();
+            },
+            (error) => {
+              reject(error);
+            }
+          );
+        }
+      );
+    }
+
+  signInUser(email: string, password: string) {
+    return new Promise(
+      // tslint:disable-next-line: no-shadowed-variable
+      (resolve, reject) => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+   }
+
+   signOutUser() {
+     firebase.auth().signOut();
+   }
+}
